385f9e14114f282f0dca77d42b869e464f264a01
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-28 23:08:17 -0500

    Exercício 1 concluído

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro não negativo:");
+            string resposta = Console.ReadLine();
+            byte guardado = byte.Parse(resposta);
+            Console.WriteLine(--guardado);
+            Console.WriteLine(++guardado);
+        }
+    }
+}
