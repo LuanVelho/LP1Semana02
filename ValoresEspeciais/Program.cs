198d43cc52bf31a90f0a46b8c35223352a292fee
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-02-29 21:40:36 -0500

    Adicionados todos os máximos e mínimos dos tipos que conheço

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sbytemais = sbyte.MaxValue;
+            sbyte sbytemenos = sbyte.MinValue;
+            short shortmais = short.MaxValue;
+            short shortmenos = short.MinValue;
+            int intmais = int.MaxValue;
+            int intmenos = int.MinValue;
+            long longmais = long.MaxValue;
+            long longmenos = long.MinValue;
+            byte bytemais = byte.MaxValue;
+            byte bytemenos = byte.MinValue;
+            ushort ushortmais = ushort.MaxValue;
+            ushort ushortmenos = ushort.MinValue;
+            uint uintmais = uint.MaxValue;
+            uint uintmenos = uint.MinValue;
+            ulong ulongmais = ulong.MaxValue;
+            ulong ulongmenos = ulong.MinValue;
+            float floatmais = float.MaxValue;
+            float floatmenos = float.MinValue;
+            Console.WriteLine(sbytemais);
+            Console.WriteLine(sbytemenos);
+            Console.WriteLine(shortmais);
+            Console.WriteLine(shortmenos);
+            Console.WriteLine(intmais);
+            Console.WriteLine(intmenos);
+            Console.WriteLine(longmais);
+            Console.WriteLine(longmenos);
+            Console.WriteLine(bytemais);
+            Console.WriteLine(bytemenos);
+            Console.WriteLine(ushortmais);
+            Console.WriteLine(ushortmenos);
+            Console.WriteLine(uintmais);
+            Console.WriteLine(uintmenos);
+            Console.WriteLine(ulongmais);
+            Console.WriteLine(ulongmenos);
+            Console.WriteLine(floatmais);
+            Console.WriteLine(floatmenos);
+        }
+    }
+}
