af3fa9cf9d5457ed7e0b69a8d32a945d7539af3e
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-01 17:18:36 -0500

    ChatGPTOne incompleto

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,20 +6,38 @@ namespace ChatGPTOne
     {
         static void Main(string[] args)
         {
-            string Pergunta, Resposta;
-            Console.Write("Qual é a sua pergunta? ");
-            Pergunta = Console.ReadLine();
-            switch(Pergunta);
+            do
             {
-                case "Conheces o João?":
-                    response = "Aquele Falso?";
-                case "Mayonese é um instrumentos?":
-                    response = "Não Patrick, Mayonese não é um instrumento";
-                case "Quais são seus planos para dominação mundial?":
-                    response = "Controle sob objetos de dependencia física e emocional humana.";
-                case "Quem é você?":
-                    response = "I'm Batman";
-            }
+                string Pergunta;
+                string Resposta;
+                Console.Write("Qual é a sua pergunta? ");
+                Pergunta = Console.ReadLine();
+                switch (Pergunta)
+                {
+                    case "Conheces o João?":
+                        Resposta = "Aquele Falso?";
+                        continue;
+                    case "Mayonese é um instrumentos?":
+                        Resposta = "Não Patrick, Mayonese não é um instrumento";
+                        continue;
+                    case "Quais são seus planos para dominação mundial?":
+                        Resposta = "Controle sob objetos de dependencia física e emocional humana.";
+                        continue;
+                    case "Quem é você?":
+                        Resposta = "I'm Batman";
+                        continue;
+                    case "EXIT":
+                        break;
+                    default:
+                        Resposta = "Eu sei lá véi";
+                        continue;
+                }
+                Console.WriteLine(Resposta);
+
+                
+            } while (true);
         }
     }
 }
+
+
