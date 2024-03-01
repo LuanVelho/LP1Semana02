d1da7eb16945ec28d6eaf0cd17ba34f021150460
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-01 16:01:21 -0500

    Perguntas adicionadas

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Pergunta, Resposta;
+            Console.Write("Qual é a sua pergunta? ");
+            Pergunta = Console.ReadLine();
+            switch(Pergunta);
+            {
+                case "Conheces o João?":
+                    response = "Aquele Falso?";
+                case "Mayonese é um instrumentos?":
+                    response = "Não Patrick, Mayonese não é um instrumento";
+                case "Quais são seus planos para dominação mundial?":
+                    response = "Controle sob objetos de dependencia física e emocional humana.";
+                case "Quem é você?":
+                    response = "I'm Batman";
+            }
+        }
+    }
+}
